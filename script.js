1498a8ff0729aeeab1d2c38b8448eabfeb61ac8d
SFurda <djswjat@gmail.com>
2021-03-05 17:29:19 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,15 @@
+function placeSelector() {
+   // let place = document.querySelector('#typePainting').value;
+
+    if (document.querySelector("#place").value == 'Interior') {
+
+        alert();
+
+    } else {
+        ////
+       
+    }
+
+
+
+}
